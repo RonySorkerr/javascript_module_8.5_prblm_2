0ea8f6d5f85ad53c9c71dcf7d777b94f9f045871
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-04 20:28:08 +0600

    max using Math.max

--- max.js
+++ max.js
@@ -1,15 +1,38 @@
 // maximum of 3 numbers
 
-const rahim = 45;
-const jamal = 69;
-const karim = 70;
+// if(rahim > jamal && rahim > karim){
+    //     console.log('rahim is bigger', rahim);
+    // }
+    // else if(jamal > rahim && jamal > karim){
+        //     console.log('jamal is bigger', jamal);
+        // }
+        // else{
+            //     console.log('karim is biggest', karim);
+            // }
+            
+            // DOING IT BY MYSELF
+            
+            function maxOfThree(rahim, jamal, karim){
+                if(rahim > jamal && rahim > karim){
+                    console.log('rahim is biggers');
+                }
+                else if(jamal > rahim && jamal > karim){
+                    console.log('jamal is the king');
+                }
+                else{
+                    console.log('Karim is the BOSSSSSS.');
+                }
+                return maxOfThree;
+            }
+            
+            const rahim = 45;
+            const jamal = 609;
+            const karim = 70;
+            const rakamma = 466;
+            const hakim = 4545;
 
-if(rahim > jamal && rahim > karim){
-    console.log('rahim is bigger', rahim);
-}
-else if(jamal > rahim && jamal > karim){
-    console.log('jamal is bigger', jamal);
-}
-else{
-    console.log('karim is biggest', karim);
-}
+            const values = maxOfThree(rahim, jamal, karim);
+            
+            const max = Math.max(rahim, jamal, karim, rakamma, hakim);
+            console.log(max);
+            
