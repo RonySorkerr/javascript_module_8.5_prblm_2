8d30fd20b39c008ce2c9105e290968e268c7b745
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-04 21:41:47 +0600

    adding

---
+++ online.js
@@ -0,0 +1,16 @@
+const lists = [
+    {name: 'Sunglass', price: 450},
+    {name: 'Shampoo', price: 250},
+    {name: 'Flower', price: 750},
+]
+
+function priceList(items){
+    let total = 0;
+    for(const item of items){
+        total = total + item.price;
+    }
+    return total;
+}
+
+const price = priceList(lists);
+console.log('the price of your items is : ' , price);
