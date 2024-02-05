ca3283442a4e29c4d1871dbd79d4763b305d0484
RonySorkerr <mdronysarkar33299@gmail.com>
2024-02-05 11:14:03 +0600

    calculator with if else

--- calculator.js
+++ calculator.js
@@ -1,1 +1,29 @@
- 
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+}
+
+function subs(a, b) {
+    const subs = a - b;
+    return subs;
+}
+
+function myl(a, b) {
+    const mul = a * b;
+    return mul;
+}
+
+function div(a, b) {
+    const div = a / b;
+    return div;
+}
+
+function calculator(a, b, opeation) {
+    if (opeation === 'add') {
+        const result = add(a, b);
+        return result;
+    }
+}
+
+const output = calculator(4, 5, 'add');
+console.log(output);
